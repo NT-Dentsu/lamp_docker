4d4d00b24336d5ca62843de16cbaed79a6c9cf46
towa1204 <fdsfgjkal@gmail.com>
2021-12-18 13:55:27 +0900

    テーブル作成クエリが変更されたことに伴う更新

--- db/initdb.d/insert_preset_data.sql
+++ db/initdb.d/insert_preset_data.sql
@@ -4,8 +4,6 @@
 # m_userのuser_id名において/^preset.*$/は既に予約されているものとする(入力チェックで弾く)
 
 # preset001の作成
-USE dentsudb;
-
 insert into t_user_panel_info values ("preset001", "size M hol", 0, 4);
 insert into t_user_panel_info values ("preset001", NULL, 1, NULL);
 insert into t_user_panel_info values ("preset001", "size S", 2, 5);
@@ -23,8 +21,8 @@ insert into t_user_panel_info values ("preset001", "size M hol", 13, 4);
 insert into t_user_panel_info values ("preset001", NULL, 14, NULL);
 insert into t_user_panel_info values ("preset001", "size S", 15, 5);
 
-# preset001に使うパネル(m_panel)の作成
-insert into m_panel values ("size L", "/app/kindle_copy/kindle_copy.php", "/images/image_panel_L.jpg");
-insert into m_panel values ("size M hol", "/app/kindle_copy/kindle_copy.php", "/images/image_panel_M_h.jpg");
-insert into m_panel values ("size M var", "/app/kindle_copy/kindle_copy.php", "/images/image_panel_M_v.jpg");
-insert into m_panel values ("size S", "/app/kindle_copy/kindle_copy.php", "/images/image_panel_S.jpg");
+# preset001に使うパネル(m_panel)の作成(絶対パスのほうが良さそう)
+insert into m_panel values ("size L", "./app/kindle_copy/kindle_copy.php", "./images/image_panel_L.jpg");
+insert into m_panel values ("size M hol", "./app/kindle_copy/kindle_copy.php", "./images/image_panel_M_h.jpg");
+insert into m_panel values ("size M var", "./app/kindle_copy/kindle_copy.php", "./images/image_panel_M_v.jpg");
+insert into m_panel values ("size S", "./app/kindle_copy/kindle_copy.php", "./images/image_panel_S.jpg");
