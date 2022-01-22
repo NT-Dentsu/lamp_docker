7dfc9207235c216f773341034bb361aab4700353
towa1204 <fdsfgjkal@gmail.com>
2022-01-22 19:25:49 +0900

    destination_dirを削除し、lamp_dockerディレクトリへcdで遷移するように変更

--- copy.sh
+++ copy.sh
@@ -2,12 +2,12 @@
 
 set -euo pipefail
 
-# source_dirとdestination_dirに値を設定してください
+# source_dirに値を設定してください
 
 # dentsuToolディレクトリがあるところまでのパス(dentsuTool/は含まなくていい)
 source_dir="~/dentsu/"
-# lamp-dockerディレクトリがあるところまでのパス(lamp/は含まなくていい)
-destination_dir="~/dentsu/"
+# copy.sh が配置されているディレクトリをカレントディレクトリとする
+cd `dirname $0`
 
 # dentsuTools/からuser_icon/までのパス
 usericon_dir="index/content/user_icon/"
@@ -23,15 +23,15 @@ trap error_handler ERR
 set -x
 
 # セッションファイルの保存先ディレクトリをwww-dataが書き込めるように権限変更
-eval "chmod o+rwx ${destination_dir}lamp_docker/web/phpsession/"
+eval "chmod o+rwx ./web/phpsession/"
 # indexディレクトリ以下をlamp/web/html/にコピー
-eval "cp -r ${source_dir}dentsuTools/index/ ${destination_dir}lamp_docker/web/html/"
+eval "cp -r ${source_dir}dentsuTools/index/ ./web/html/"
 # アイコンの保存先ディレクトリにwww-dataがファイルを作成できるように権限変更
-eval "chmod o+rwx ${destination_dir}lamp_docker/web/html/${usericon_dir}"
+eval "chmod o+rwx ./web/html/${usericon_dir}"
 
 # dbaccess.php中にあるデータベースのホスト名をlocalhostからdbに変更
 before_host="const HOST_NAME = 'localhost'"
 after_host="const HOST_NAME = 'db'"
-eval "sed -i -e \"s/${before_host}/${after_host}/\" ${destination_dir}lamp_docker/web/html/index/common/dbaccess.php"
+eval "sed -i -e \"s/${before_host}/${after_host}/\" ./web/html/index/common/dbaccess.php"
 set +x
 echo "successful"
