3ea97ae4f902d5e3c1c85dd9b5bdff99f39e09d6
towa1204 <lsadsfj@gmail.com>
2022-07-16 23:11:46 +0900

    リファクタリング&バグ修正

--- copy.sh
+++ copy.sh
@@ -2,12 +2,15 @@
 
 set -euo pipefail
 
+# このスクリプト自身が置かれているディレクトリに移動する
+THIS_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # source_dirに値を設定してください
 
 # dentsuToolディレクトリがあるところまでのパス(dentsuTool/は含まなくていい)
-source_dir="~/dentsu/"
+source_dir="$HOME/web_develop/dentsu/"
 # copy.sh が配置されているディレクトリをカレントディレクトリとする
-cd `dirname $0`
+cd "$THIS_SCRIPT_DIR"
 
 # dentsuTools/からuser_icon/までのパス
 usericon_dir="index/content/user_icon/"
@@ -25,7 +28,9 @@ set -x
 # セッションファイルの保存先ディレクトリをwww-dataが書き込めるように権限変更
 eval "chmod o+rwx ./web/phpsession/"
 # 古いindexディレクトリを削除
-eval "rm -r ./web/html/index/"
+if [ -d ./web/html/index ]; then
+  eval "rm -r ./web/html/index/*"
+fi
 # indexディレクトリ以下をlamp/web/html/にコピー
 eval "cp -r ${source_dir}dentsuTools/index/ ./web/html/"
 # アイコンの保存先ディレクトリにwww-dataがファイルを作成できるように権限変更
